a7b350f85b11e12e78f2accbfddfc01d9bb4a862
Pedro Couto <pedrocouto39@example.com>
2021-07-23 10:52:27 +0200

    refactor: add source database feature which allows the user to chose where the source table is located.

--- 5_Data_Pipeline_with_Airflow/plugins/helpers/sql_queries.py
+++ 5_Data_Pipeline_with_Airflow/plugins/helpers/sql_queries.py
@@ -11,9 +11,9 @@ class SqlQueries:
                 events.location, 
                 events.useragent
                 FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
-            FROM staging_events
+            FROM {source_database}.staging_events
             WHERE page='NextSong') events
-            LEFT JOIN staging_songs songs
+            LEFT JOIN {source_database}.staging_songs songs
             ON events.song = songs.title
                 AND events.artist = songs.artist_name
                 AND events.length = songs.duration
@@ -21,22 +21,22 @@ class SqlQueries:
 
     user_table_insert = ("""
         SELECT distinct userid, firstname, lastname, gender, level
-        FROM staging_events
+        FROM {source_database}.staging_events
         WHERE page='NextSong'
     """)
 
     song_table_insert = ("""
         SELECT distinct song_id, title, artist_id, year, duration
-        FROM staging_songs
+        FROM {source_database}.staging_songs
     """)
 
     artist_table_insert = ("""
         SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
-        FROM staging_songs
+        FROM {source_database}.staging_songs
     """)
 
     time_table_insert = ("""
         SELECT start_time, extract(hour from start_time), extract(day from start_time), extract(week from start_time), 
                extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
-        FROM songplays
+        FROM {source_database}.songplays
     """)
